0000313bec54acd2d155e0f567000699717fd730
Wells-Alex <wells_@outlook.com>
2024-12-08 19:07:17 +0100

    initial commit

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -2,5 +2,6 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.print("HelloGoIt");
+        System.out.println("Ok");
     }
 }
